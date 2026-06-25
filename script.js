af3172481cc2e13bab5be05f9164a8e6df080256
gtr-1000 <dalbertvinicius5@gmail.com>
2026-06-25 20:28:08 -0300

    Correct blend name assignment conditions
    Fix conditional logic for blend name assignment.

--- script.js
+++ script.js
@@ -16,12 +16,26 @@ button.addEventListener("click", () => {
 
     if (intensity.value === "light" &&  method.value === "french"){
         blendName.textContent = "AURORA";}
+    if (intensity.value === "light" && method.value === "espresso"){
+        blendName.textContent = "KRIN";}
+    if (intensity.value === " light" &&& method.value === "v60"){
+        blendName.textContent === "FLUE";}
     if (intensity.value === "medium" && method.value === "v60"){
         blendName.textContent = "VELVET";}
+    if (intensity.value === "medium" && method.value === "espresso"){
+        blendName.textContent = "LORT";}
+    if (intensity.value === "medium" && method.value === "french"){
+        blendName.textContent = "MAGIF";}
     if (intensity.value === "strong"  && method.value === "espresso"){
         blendName.textContent = "ECLIPSE";}
     if (intensity.value === "strong" && method.value === "french"){
         blendName.textContent = "RITUAL";}
+    if (intensity.value === "strong" && method.value === "v60"){
+        blendName.textContent = "VIKING";}
     if (intensity.value === "very strong" && method.value === "french"){
         blendName.textContent = "VL200";}
+    if (intensity.value === "very strong" && method.value === "espresso"){
+        blendName.textContent = "WARRIORS";}
+    if (intensity.value === "very strong" && method.value === "v60"){
+        blendName.textContent = "CHARM";}
 });
