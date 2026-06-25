71edcbc90b1f533919010d4bae3c6e4f1a9132fe
gtr-1000 <dalbertvinicius5@gmail.com>
2026-06-25 19:59:30 -0300

    Add method checks for blend name selection

--- script.js
+++ script.js
@@ -6,14 +6,20 @@ button.addEventListener("click", () => {
         'input[name="intensity"]:checked'
     );
 
+    const method = document.querySelector( 
+
+        'input[name="method"]:checked' 
+
+    );
+
     const blendName = document.getElementById("blend-name");
 
-    if (intensity.value === "light"){
+    if (intensity.value === "light" &&  method.value === "french"){
         blendName.textContent = "AURORA";}
-    if (intensity.value === "medium"){
+    if (intensity.value === "medium" && method.value === "v60"){
         blendName.textContent = "VELVET";}
-    if (intensity.value === "strong"){
+    if (intensity.value === "strong"  && method.value === "expresso"){
         blendName.textContent = "ECLIPSE";}
-    if(intensity.value === "very strong"){
+    if (intensity.value === "very strong" && method.value === "expresso"){
         blendName.textContent = "VL200";}
 });
