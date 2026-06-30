8192eb86def9bff7c0d0186847d75e37ac666cf2
gtr-1000 <dalbertvinicius5@gmail.com>
2026-06-30 13:47:35 -0300

    Refactor recommendation logic to use else if

--- script.js
+++ script.js
@@ -18,27 +18,27 @@ button.addEventListener("click", () => {
 
     if (intensity.value === "light" &&  method.value === "french"){
         recommendation = "AURORA";}
-    if (intensity.value === "light" && method.value === "espresso"){
+    else if (intensity.value === "light" && method.value === "espresso"){
         recommendation = "KRIN";}
-    if (intensity.value === "light" && method.value === "v60"){
+    else if (intensity.value === "light" && method.value === "v60"){
         recommendation = "FLUE";}
-    if (intensity.value === "medium" && method.value === "v60"){
+    else if (intensity.value === "medium" && method.value === "v60"){
         recommendation = "VELVET";}
-    if (intensity.value === "medium" && method.value === "espresso"){
+    else if (intensity.value === "medium" && method.value === "espresso"){
         recommendation = "LORT";}
-    if (intensity.value === "medium" && method.value === "french"){
+    else if (intensity.value === "medium" && method.value === "french"){
         recommendation = "MAGIF";}
-    if (intensity.value === "strong"  && method.value === "espresso"){
+    else if (intensity.value === "strong"  && method.value === "espresso"){
         recommendation = "ECLIPSE";}
-    if (intensity.value === "strong" && method.value === "french"){
+    else if (intensity.value === "strong" && method.value === "french"){
         recommendation = "RITUAL";}
-    if (intensity.value === "strong" && method.value === "v60"){
+    else if (intensity.value === "strong" && method.value === "v60"){
         recommendation = "VIKING";}
-    if (intensity.value === "very strong" && method.value === "french"){
+    else if (intensity.value === "very strong" && method.value === "french"){
         recommendation = "VL200";}
-    if (intensity.value === "very strong" && method.value === "espresso"){
+    else if (intensity.value === "very strong" && method.value === "espresso"){
         recommendation = "WARRIORS";}
-    if (intensity.value === "very strong" && method.value === "v60"){
+    else {
         recommendation = "CHARM";}
 
 
